c4e73202cb2da35ca84f5538b83e2829e25e8d3e
Bdub <bheadwhite@gmail.com>
2018-04-10 13:46:28 -0600

    toy problem step 2 done

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class EvenAndOdd extends Component {
+    render(){
+        return (
+            <p>Even and odd </p>
+        )
+    }
+}
+
+export default EvenAndOdd;
