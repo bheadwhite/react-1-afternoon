29821d1746f163f2c0f3e39f1d3bb3f05bae5ea7
Bdub <bheadwhite@gmail.com>
2018-04-11 13:12:57 -0600

    finished object started string

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react'
 
 class FilterObject extends Component {
+    constructor(){
+        super()
+        this.state = { 
+            employees: [
+                {
+                  name: 'Jimmy Joe',
+                  title: 'Hack0r',
+                  age: 12,
+                },
+                {
+                  name: 'Jeremy Schrader',
+                  age: 24,
+                  hairColor: 'brown'
+                },
+                {
+                  name: 'Carly Armstrong',
+                  title: 'CEO',
+                }
+              ],
+            userInput: '',
+            filteredEmployees: [],
+
+        }
+    }
+    handleChange(val){
+        this.setState({userInput: val})
+    }
+    filterEmployees(prop){
+        let bing = this.state.employees.filter(item => item[prop])
+        this.setState({filteredEmployees: bing})
+    }
+
     render(){
         return (
-            <p>Filter Object</p>
+
+            <div className="puzzleBox filterObjectPB">
+            <h4>Filter Object</h4>
+            <span className="puzzletext">Original: {JSON.stringify(this.state.employees, null, 10)}</span>
+            <input className="inputLine" onChange={ (e) => {this.handleChange(e.target.value)}}></input>
+            <button className="confirmationButton" onClick= {()=> this.filterEmployees(this.state.userInput)}> Filter </button>
+            <span className="resultsBox filterObjectRB">Filtered: {JSON.stringify(this.state.filteredEmployees, null, 10)}</span>
+            </div>
         )
     }
 }
--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react'
 class FilterString extends Component {
     render(){
         return (
-            <p>FilterString</p>
+            <div className="puzzleBox filterStringPB">
+            <span className="puzzleText"></span>
+                <input className="inputLine"></input>
+                <button className="confirmationButton"></button>
+                <span className="resultsBox filterStringRB"></span>
+            </div>
         )
     }
 }
